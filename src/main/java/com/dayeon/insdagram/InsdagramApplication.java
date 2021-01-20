d2b3e7fc8d3953e3d0c2c52693b6d4055257bdbf
DayeonOh <dayeon.dev@gmail.com>
2021-01-20 22:28:23 +0900

    init

---
+++ src/main/java/com/dayeon/insdagram/InsdagramApplication.java
@@ -0,0 +1,13 @@
+package com.dayeon.insdagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsdagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsdagramApplication.class, args);
+	}
+
+}
