5aca3397e327c6c45d2c90f99d3616699e1d44d0
DayeonOh <dayeon.dev@gmail.com>
2021-01-26 23:20:11 +0900

    Add h2db connection code

---
+++ src/main/java/com/dayeon/insdagram/repository/JpaAccountRepository.java
@@ -0,0 +1,2 @@
+package com.dayeon.insdagram.repository;public class JpaAccountRepository {
+}
---
+++ src/main/java/com/dayeon/insdagram/repository/SpringDataJpaAccountRepository.java
@@ -0,0 +1,2 @@
+package com.dayeon.insdagram.repository;public interface SpringDataJpaAccountRepository {
+}
